d5a586ab303f64befede78cfaef4906dc76e9ea7
imjlk <i@m.junglei.kim>
2023-08-06 17:26:49 +0900

    realtime tweets

--- components/Tweets.tsx
+++ components/Tweets.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import TweetLikes from "./TweetLikes";
-import { experimental_useOptimistic as useOptimistic } from "react";
+import { useEffect, experimental_useOptimistic as useOptimistic } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
   const [optimisticTweets, addOptimisticTweet] = useOptimistic<
@@ -15,6 +17,32 @@ export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
     newOptimisticTweets[index] = newTweet;
     return newOptimisticTweets;
   });
+
+  const supabase = createClientComponentClient<Database>();
+  const router = useRouter();
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("realtime tweets")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "tweets",
+        },
+        (payload) => {
+          router.refresh();
+          // console.log(payload);
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase, router]);
+
   return optimisticTweets.map((tweet) => (
     <div key={tweet.id} className="flex flex-col items-center mb-4 lg:mb-4">
       <p className="flex gap-8 justify-center items-center">{tweet?.title}</p>
